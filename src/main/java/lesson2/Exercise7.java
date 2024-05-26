896218a0159044cb27a910e9e787395b2247a37d
Alema <akryvalap@gmail.com>
2024-05-26 22:29:13 +0200

    Exercise 7

---
+++ src/main/java/lesson2/Exercise7.java
@@ -0,0 +1,10 @@
+package src.main.java.lesson2;
+
+public class Exercise7 {
+    public static void main(String[] args) {
+        String one ="я первый стринг ";
+        System.out.println(one);
+        String two = "я второй стринг";
+        System.out.println(one+two);
+    }
+}
