322ddfccd0270e664f1c043f4aa5809c827b31b0
Alema <akryvalap@gmail.com>
2024-05-26 21:53:15 +0200

    Exercise 4

---
+++ src/main/java/lesson2/Exercise4.java
@@ -0,0 +1,16 @@
+package src.main.java.lesson2;
+
+public class Exercise4 {
+    public static void main(String[] args) {
+        int number = 3;
+        int number2 = 8;
+
+        if (number > number2) {
+            System.out.println(number);
+        } else if (number < number2) {
+            System.out.println(number2);
+        } else if (number == number2) {
+            System.out.println(number);
+        }
+    }
+}
