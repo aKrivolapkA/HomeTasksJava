02865b38744536309cf7a334bf22bd519f2af642
Alema <akryvalap@gmail.com>
2024-05-26 22:18:11 +0200

    Exercise 6

---
+++ src/main/java/lesson2/Exercise6.java
@@ -0,0 +1,24 @@
+package src.main.java.lesson2;
+
+public class Exercise6 {
+    public static void main(String[] args) {
+        byte byteVar = 127;
+        short shortVar = 32767;
+        int intVar = 2147483647;
+        long longVar = 9223372036854775807L;
+        float floatVar = 3.14f;
+        double doubleVar = 3.141592653589793;
+        char charVar = 'A';
+        boolean booleanVar = true;
+
+        System.out.println("Примитивные типы данных:");
+        System.out.println("byte: " + byteVar);
+        System.out.println("short: " + shortVar);
+        System.out.println("int: " + intVar);
+        System.out.println("long: " + longVar);
+        System.out.println("float: " + floatVar);
+        System.out.println("double: " + doubleVar);
+        System.out.println("char: " + charVar);
+        System.out.println("boolean: " + booleanVar);
+    }
+}
