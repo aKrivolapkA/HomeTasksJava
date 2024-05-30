c52de47f0ecdcd3861fa5e00bc3b9a7d637dfb90
Alema <akryvalap@gmail.com>
2024-05-30 14:27:00 +0200

    Массивы 1

---
+++ src/main/java/lesson3/Arrays1.java
@@ -0,0 +1,41 @@
+package src.main.java.lesson3;
+
+
+public class Arrays1 {
+    public static void main(String[] args) {
+    /*
+    Создайте массив из всех чётных чисел от 2 до 20 и выведите элементы массива на экран сначала в строку, отделяя один элемент от другого пробелом,
+     а затем в столбик (отделяя один элемент от другого началом новой строки).
+     Перед созданием массива подумайте, какого он будет размера.
+     */
+
+        int size = 0;
+        for (int i = 2; i < 20; i++)
+            if (i % 2 == 0) {
+                size++;
+            }
+        size++;
+
+        System.out.println("размер массива: " + size);
+
+        System.out.println("--------------------");
+
+        int[] array1 = new int[size];
+        for (int i = 0, number = 2; number <= 20; number += 2, i++) {
+            array1[i] = number;
+        }
+
+        for (int i = 0; i < array1.length; i++) {
+            System.out.print(array1[i]);
+        }
+        System.out.println();
+        System.out.println("--------------------");
+
+
+        for (int number : array1) {
+            System.out.println(number);
+        }
+    }
+
+}
+
