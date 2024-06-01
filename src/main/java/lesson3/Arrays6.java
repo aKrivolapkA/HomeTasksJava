4bb2f24da4d8001a3dc93036973b3b27eadc2eba
Alema <akryvalap@gmail.com>
2024-06-01 19:56:56 +0200

    Exercise 6

---
+++ src/main/java/lesson3/Arrays6.java
@@ -0,0 +1,28 @@
+package src.main.java.lesson3;
+
+import java.util.Arrays;
+import java.util.Random;
+
+/*
+Создайте массив из 4 случайных целых чисел из отрезка [0;10],
+выведите его на экран в строку.
+ Определить и вывести на экран сообщение о том, является ли массив строго возрастающей последовательностью.
+
+ */
+public class Arrays6 {
+    public static void main(String[] args) {
+
+        int[] array = new int[4];
+        Random random=new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i]=random.nextInt(10);
+        }
+        System.out.println("Массив в строку: "+ Arrays.toString(array));
+
+        if (array[0]<array[1] &&array[1]<array[2]&&array[3]<array[4]){
+            System.out.println("массив является  строго возрастающей последовательностью.");
+        }else {
+            System.out.println("массив не является  строго возрастающей последовательностью");
+        }
+    }
+}
